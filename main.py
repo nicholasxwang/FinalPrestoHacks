722725bfdd953c92c194507c56b1ea6276d08ac7
Nicholas Wang <80929436+nicholasxwang@users.noreply.github.com>
2021-12-12 10:00:15 +0000

    a

--- main.py
+++ main.py
@@ -4,7 +4,17 @@ display.start()
 from selenium import webdriver
 import time
 browser = webdriver.Firefox()
+from __future__ import unicode_literals
+import youtube_dl
 
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
 
 ignore = [
   "wallpaper",
@@ -33,73 +43,46 @@ ignore = [
   "and",
   "or"
 ]
-def main_program(image):
-  a = str(get_image_name(image))
-  #Check if it exists
-  if a == "n":
-    #return "Sorry, I could not find any song related to your picture. How about, try Head Up by The Score!"
-    return []
-  #split into words
-  word = a.split(" ")
-  songs = []
-  ignored = []
-  import nltk
-  nltk.download('wordnet')
-  from nltk.corpus import wordnet
-  for i in word:
-    if i.lower() in ignore:
-      ignored.append(i.lower())
-      continue
-    #Find Synonyms
-    synonyms = []
-    antonyms = []
-      
-    for syn in wordnet.synsets(i):
-        for l in syn.lemmas():
-            synonyms.append(l.name())
-            if l.antonyms():
-                antonyms.append(l.antonyms()[0].name())
-      
-    for x in range(0,len(synonyms)):
-      synonyms[x] = synonyms[x].replace("_"," ")
-    synonyms = set(synonyms)
-    #Search for songs
-    synonyms = list(synonyms)
-    synonyms = [i]+synonyms
-    
-    for j in synonyms:
-      temp = find_music(j)
-      if not (temp=="n"):
-        for j in temp:
-          songs.append(j.text)
+def main_program(file_name):
+  import io
+  import os
+  os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="./festive-freedom-309323-0124a1c976ae.json"
+  from google.cloud import vision
+  client = vision.ImageAnnotatorClient()
+  file_name = os.path.abspath(file_name)
+  # Loads the image into memory
+  with io.open(file_name, 'rb') as image_file:
+      content = image_file.read()
+  image = vision.Image(content=content)
 
-  if len(songs) == 0:
-    #return "I found what your picture is thanks to my smart AI but I could not find any songs. How about, try AJR's Bang? Synonyms include "+str(synonyms)+"\n We ignored: "+str(ignored)
-    return []
-  else:
-    #return "Yay! The best result is \""+str(songs[0])+"\" \nOur Synonym list include: " + str(synonyms)+"\nOther results include: "+str(songs[1:len(songs)])+"\n We ignored: "+str(ignored)
-    return str(songs)
+  #Logos
+  logo_list = []
+  response = client.logo_detection(image=image)
+  logos = response.logo_annotations
+  for logo in logos:
+      logo_list.append(logo.description)
 
-def get_image_name(image_url):
-  browser.get("https://www.google.com/searchbyimage?site=search&sa=X&image_url="+image_url)
-  browser.get_screenshot_as_file('/static/debug.png') 
-  print("took the image")
-  try:
-    parsed = browser.find_elements_by_class_name("fKDtNb")[0].text
-  except Exception as e:
-    parsed = "n"
-    print(e)
-  return redirect("/static/debug.png")
+  if (len(logo_list)>0):
+    return logo_list
+  response = client.landmark_detection(image=image)
+  landmarks = response.landmark_annotations
+  landmark_list = []
+  for landmark in landmarks:
+      landmark_list.append(landmark.description)
 
-def find_music(query):
-  browser.get(f"https://www.musixmatch.com/search/{query}/tracks")
-  time.sleep(2)
-  try:
-    a = browser.find_elements_by_class_name("title")
-  except:
-    return "n"
-  return a
-  
+
+  if (len(landmark_list)>0):
+    return landmark_list
+
+  #General
+  response = client.label_detection(image=image)
+  labels = response.label_annotations
+  general_list = []
+
+  for label in labels:
+      general_list.append(label.description)
+
+  return general_list
 
 
 #Permissions Fix: chmod +x geckodriver
@@ -127,7 +110,7 @@ def main():
   return render_template('index.html')
 app.secret_key = 'github'
 UPLOAD_FOLDER = './static/images'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'heic', 'webm'}
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -138,24 +121,20 @@ def upload_file():
         #return str(request.files)
         # check if the post request has the file part
         if 'file' not in request.files:
-            #return '1'
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            #return '2'
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            #return '3'
             filename = secure_filename(file.filename)
-            #os.mkdir(os.path.join(UPLOAD_FOLDER, filename))
             file.save(os.path.join(UPLOAD_FOLDER, filename))
-            x=os.path.join(UPLOAD_FOLDER, filename)
-            #return redirect(url_for('upload_file', name=filename))
-            print(get_image_name(x))
-            return str(get_image_name(x))
+            a = main_program(os.path.join(UPLOAD_FOLDER, filename))
+            return str(a)
+
+            
 #Run
 serve(app, host="0.0.0.0", port=8080)
